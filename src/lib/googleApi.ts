be5ce2fcf2b8b24f71eeecb5c5d686232fe08381
Himanshu Chaudhary <himanshuchoudhary856@gmail.com>
2025-05-25 15:05:04 +0530

    add: redirect uri

--- src/lib/googleApi.ts
+++ src/lib/googleApi.ts
@@ -25,7 +25,7 @@ export async function createGoogleMeetLink({
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
-      process.env.NEXTAUTH_URL
+      `${process.env.NEXTAUTH_URL}/api/auth/callback/google`
     )
     
     oauth2Client.setCredentials({ 
